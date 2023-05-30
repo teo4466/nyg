1960d3c4a0eff21097d0f24f468e2b092018b472
gonzalo09 <gonzalomachado09@gmail.com>
2023-05-30 15:20:24 +0200

    de

--- nyj/settings.py
+++ nyj/settings.py
@@ -37,13 +37,14 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   
+    
     'rest_framework',  
     'project',
     
