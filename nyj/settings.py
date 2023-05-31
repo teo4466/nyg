ffdab9620d0359ed4e96604b4423132e067f6792
gonzalo09 <gonzalomachado09@gmail.com>
2023-05-31 19:57:28 +0200

    ff

--- nyj/settings.py
+++ nyj/settings.py
@@ -133,10 +133,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
     "https://splendid-kelpie-0ea91f.netlify.app",
-    "https://nyglog.netlify.app/",
-    
-    
-
     
     
 ]
