32020ca4d5a3eb70ab53fd0fdf32282faf9c14ed
gonzalo09 <gonzalomachado09@gmail.com>
2023-06-04 01:44:47 +0200

    saque

--- project/urls.py
+++ project/urls.py
@@ -7,7 +7,7 @@ router = routers.DefaultRouter()
 router.register("api/project", ClientesViewSet, "project")
 router.register('api/departments', DepartmentsViewSet,"departments")
 router.register('api/empleados', EmpleadosViewSet,"empleados")
-router.register('api/perfile', PerfilempleadoViewSet,"perfile")
+"""router.register('api/perfile', PerfilempleadoViewSet,"perfile")
 router.register('api/accesoriose', AccesoriosempleadoViewSet,"accesoriose")
 router.register('api/accesorioent', AccesorioentregadoViewset,"accesorioent")
 router.register('api/habilitaciones', HabilitacionesempleadoViewSet,"habilitaciones")
@@ -20,6 +20,6 @@ router.register('api/accesoriosc', AccessoriocamionViewSet,"accesoriosc")
 router.register('api/checks', CheckcamionViewSet,"checks")
 router.register('api/mantenimientoc', MantenimientocamionViewSet,"mantenimientoc")
 router.register('api/service', ServicecamionViewset,"service")
-router.register('api/cubiertas', CubiertasViewSet,"cubiertas")
+router.register('api/cubiertas', CubiertasViewSet,"cubiertas")"""
 
 urlpatterns = router.urls
