f905152599c9a18191165d46cb58b2292af3f7c6
gonzalo09 <gonzalomachado09@gmail.com>
2023-06-17 16:46:45 +0200

    ai

--- project/models.py
+++ project/models.py
@@ -150,8 +150,8 @@ class Cubiertas(models.Model):
     kmrotacion =models.CharField(max_length=10, default=True)
     kmrecambio = models.CharField(max_length=10, default=True)
     fechacolocacion = models.CharField(max_length=50)
-    posicion = models.CharField(max_length=2)
-    posicion2 = models.CharField(max_length=2, default=True)
+    #posicion = models.CharField(max_length=2)
+    #posicion2 = models.CharField(max_length=2, default=True)
 
 class Combustible(models.Model):
     fecha= models.CharField(max_length=50, default="")
