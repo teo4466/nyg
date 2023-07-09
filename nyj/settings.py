f7d8df9d6b833b1e97eec70ae7eaa268d011dd43
gonzalo09 <gonzalomachado09@gmail.com>
2023-07-09 16:34:37 +0200

    sta

--- nyj/settings.py
+++ nyj/settings.py
@@ -128,11 +128,11 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATIC_STORAGE ="whitenoise.storarge.CompressedManifestStaticFileStorage"
+    STATIC_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
